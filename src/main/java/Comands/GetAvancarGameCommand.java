f196a5d4de7cf5e2c2a63bb8935f38b5bc075c75
Mateus Gonçalo <mateusgon57@gmail.com>
2019-04-23 01:12:05 -0300

    Adicionando comunicação com servidor do ASR

--- src/main/java/Comands/GetAvancarGameCommand.java
+++ src/main/java/Comands/GetAvancarGameCommand.java
@@ -1,19 +1,17 @@
 package Comands;
 
-import Models.Image;
-import Mundo.Game;
-import Mundo.GameSteps;
-import Mundo.Player;
-import Persistence.GameDAO;
-import Persistence.ImageDAO;
-import Persistence.PlayerDAO;
+import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.openide.util.Exceptions;
 
 public class GetAvancarGameCommand implements Comando {
@@ -21,7 +19,41 @@ public class GetAvancarGameCommand implements Comando {
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
         try {
-            Integer id = Integer.parseInt(request.getParameter("id"));
+
+            OkHttpClient client = new OkHttpClient.Builder().connectTimeout(120, TimeUnit.SECONDS).
+                    writeTimeout(120, TimeUnit.SECONDS).readTimeout(120, TimeUnit.SECONDS).build();
+            
+            Properties config = new Properties();
+            config.load(request.getServletContext().getResourceAsStream("/WEB-INF/properties/config.properties"));
+
+            File uploads = new File(config.getProperty("UPLOAD_DIR"));
+            if (!uploads.exists()) {
+                uploads.mkdirs();
+            }
+
+            File som = null;
+            File[] sons = uploads.listFiles();
+            for (File f : sons) {
+                if (f.getAbsoluteFile().getName().equals("palito.wav")) {
+                    som = f;
+                }
+            }
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("text", "palitó")
+                    .addFormDataPart("audio", "audio.wav",
+                            RequestBody.create(MediaType.get("audio/wav"), som))
+                    .build();
+
+            Request request2 = new Request.Builder()
+                    .url("http://200.131.219.35:8085/")
+                    .post(requestBody)
+                    .build();
+            Response response2 = client.newCall(request2).execute();
+            System.out.println(response2.body().string());
+
+            /*Integer id = Integer.parseInt(request.getParameter("id"));
             List<Image> images = ImageDAO.getInstance().listImages(id);
             Game game = GameDAO.getInstance().searchGame(id);
             GameSteps avancarGame = new GameSteps();
@@ -40,9 +72,8 @@ public class GetAvancarGameCommand implements Comando {
             else
             {
                 System.out.println("Final de partida");
-            }
-
-        } catch (IOException | ServletException | SQLException | ClassNotFoundException ex) {
+            }*/
+        } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         }
     }
