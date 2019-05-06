773dd0971b1ac752bf3244930731236275f5e525
Igor Knop <igorknop@gmail.com>
2019-05-06 05:26:03 -0300

    Adiciona classe Node para ler do JSON

---
+++ src/main/java/Mundo/Node.java
@@ -0,0 +1,43 @@
+package Mundo;
+
+public class Node {
+
+    private Integer id;
+    private Integer x;
+    private Integer y;
+
+    public Node() {
+        this(null, null, null);
+    }
+
+    public Node(Integer id, Integer x, Integer y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+}
---
+++ src/test/java/Mundo/NodeTest.java
@@ -0,0 +1,102 @@
+package Mundo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Node.
+     */
+    @Test
+    public void testGetId() {
+        System.out.println("getId");
+        Node instance = new Node();
+        Integer expResult = null;
+        Integer result = instance.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class Node.
+     */
+    @Test
+    public void testSetId() {
+        System.out.println("setId");
+        Integer id = 1;
+        Node instance = new Node();
+        instance.setId(id);
+    }
+
+    /**
+     * Test of getX method, of class Node.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+        Node instance = new Node();
+        Integer expResult = null;
+        Integer result = instance.getX();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setX method, of class Node.
+     */
+    @Test
+    public void testSetX() {
+        System.out.println("setX");
+        Integer x = 10;
+        Node instance = new Node();
+        instance.setX(x);
+        assertEquals(x, instance.getX());
+    }
+
+    /**
+     * Test of getY method, of class Node.
+     */
+    @Test
+    public void testGetY() {
+        System.out.println("getY");
+        Node instance = new Node();
+        Integer expResult = null;
+        Integer result = instance.getY();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setY method, of class Node.
+     */
+    @Test
+    public void testSetY() {
+        System.out.println("setY");
+        Integer y = 20;
+        Node instance = new Node();
+        instance.setY(y);
+        assertEquals((Integer)20, instance.getY());
+    }
+    
+}
