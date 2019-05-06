893c04b8bf2bfda4a9e38a3aa57305f305f6924c
Igor Knop <igorknop@gmail.com>
2019-05-06 05:25:37 -0300

    Adiciona teste integração para Game

---
+++ src/test/java/Mundo/GameIntegrationTest.java
@@ -0,0 +1,54 @@
+package Mundo;
+
+import Grafo.Grafo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameIntegrationTest {
+    Game game;
+    
+    public GameIntegrationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        game = GameGenerator.getGameStub();
+    }
+    
+    @After
+    public void tearDown() {
+        game = null;
+    }
+
+    /**
+     * @Description Deve haver um stub antes
+     */
+    @Test
+    public void testNewStub() {
+        assertNotNull(game);
+        assertEquals((Integer)0, game.getCurrentPlayer());
+        assertEquals((Integer)8, game.getPlayers());
+    }
+    
+    @Test
+    public void testGetOptions() {
+        game.getOpcoes();
+    }
+    
+}
